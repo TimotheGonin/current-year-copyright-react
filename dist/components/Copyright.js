aaf5f1e6905cdacb5bf44d149cbef29f9bce27a4
TimotheGonin <gonin.timothe@gmail.com>
2023-07-12 20:11:16 +0200

    👷 🔥 remove useless /public, version+, build

--- dist/components/Copyright.js
+++ dist/components/Copyright.js
@@ -6,9 +6,12 @@ Object.defineProperty(exports, "__esModule", {
 exports.default = void 0;
 var _react = _interopRequireDefault(require("react"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-const Copyright = () => {
+const Copyright = _ref => {
+  let {
+    nameEntry
+  } = _ref;
   const currentYear = new Date().getFullYear();
-  return /*#__PURE__*/_react.default.createElement("p", null, "Copyright \xA9 ", currentYear);
+  return /*#__PURE__*/_react.default.createElement("p", null, "Copyright \xA9 ", currentYear, " - ", nameEntry);
 };
 var _default = Copyright;
 exports.default = _default;
